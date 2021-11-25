a3e092ab9958987fd84cd494b099e74389fc493e
PandaFood <Jonathan.Brorsson@gmail.com>
2021-11-25 18:07:57 +0100

    Test day 1

--- day01/src/main/scala/Main.scala
+++ day01/src/main/scala/Main.scala
@@ -2,24 +2,31 @@ package day1
 
 import scala.io.Source
 
-val PART =  sys.env("part")
-
 def InputFile: Array[Int] = return Source.fromFile("input.txt").getLines.map(_.toInt).toArray
 
 
 @main def Main : Unit = {
 
   val input = InputFile
-  // val input: Array[Int] = Array(12, 14, 1969, 100756)
 
-  //input.map(x => println(x))
+  val PART = sys.env.get("part") match {
+    case Some(x) => x
+    case _ => "part2"
+  }
 
   println("part: " + PART)
 
-  if PART == "part1" then
+  var answ = 0
 
-    val answ = input.map(x => ((x/3)-2)).reduce((x, y) => x + y)
-    println("Answer: " + answ)
+  if PART == "part1" then
+    answ = input.zipWithIndex.filter((x, i) => isPrime(x)).map((x, i) => x * i).reduce((x, y) => x + y)
+  else if PART == "part2" then
+    answ = input.zipWithIndex.map((x: Int, i: Int) => if (!isPrime(x) && i % 2 == 0) then x else if (!isPrime(x) && i % 2 == 1) then x * -1 else 0 ).reduce((x, y) => x + y)
+
+  println("Answer: " + answ)
 
 }
   
+def isPrime(i: Int): Boolean = if (i <= 1) false else if (i == 2) true else !(2 until i).exists(n => i % n == 0)
+
+
