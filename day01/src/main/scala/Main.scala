3937b4fd1d21a0eb580b48d5d99be104549cdfc9
PandaFood <Jonathan.Brorsson@gmail.com>
2021-12-01 06:52:53 +0100

    Day 1 solved

--- day01/src/main/scala/Main.scala
+++ day01/src/main/scala/Main.scala
@@ -19,14 +19,16 @@ def InputFile: Array[Int] = return Source.fromFile("input.txt").getLines.map(_.t
   var answ = 0
 
   if PART == "part1" then
-    answ = input.zipWithIndex.filter((x, i) => isPrime(x)).map((x, i) => x * i).reduce((x, y) => x + y)
-  else if PART == "part2" then
-    answ = input.zipWithIndex.map((x: Int, i: Int) => if (!isPrime(x) && i % 2 == 0) then x else if (!isPrime(x) && i % 2 == 1) then x * -1 else 0 ).reduce((x, y) => x + y)
+    input.reduce((x,y) => {
+    if y > x then answ += 1
+    y })
 
+  else if PART == "part2" then
+    input.zipWithIndex.map((x: Int, i: Int) => x + input((i + 1).min((input.length-1))) + input((i + 2).min((input.length-1))) ).take(input.length-2).reduce((x,y) => { if y > x then answ += 1; y })
+  
   println("Answer: " + answ)
 
 }
   
-def isPrime(i: Int): Boolean = if (i <= 1) false else if (i == 2) true else !(2 until i).exists(n => i % n == 0)
 
 
